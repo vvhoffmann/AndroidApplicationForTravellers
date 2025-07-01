70e56feadd3625bfd4205543bc6f7398e32cca21
vvhoffmann <whoffmann2000@wp.pl>
2025-07-01 12:35:24 +0200

    2.4.1 Refactor

--- app/src/main/java/pl/vvhoffmann/routemyway/config/AppConfig.java
+++ app/src/main/java/pl/vvhoffmann/routemyway/config/AppConfig.java
@@ -1,5 +1,5 @@
 package pl.vvhoffmann.routemyway.config;
 
 public class AppConfig {
-    public static final String GOOGLE_MAPS_API_KEY = "AIzaSyCdkI9RbAndWnltLwC9QHfHgOfXaNVfjYU";
+    public static final String GOOGLE_MAPS_API_KEY = "API_KEY";
 }
